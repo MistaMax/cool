3a4b9a464bafb81f900220d6a78817052be5ce57
Max Crookshanks <mmc_the_ace@yahoo.com>
2017-09-08 12:56:09 -0500

    created the mmMark function

--- cs3723/program-1-storage-management/cs3723p1.c
+++ cs3723/program-1-storage-management/cs3723p1.c
@@ -4,19 +4,35 @@
 #include <string.h>
 
 void mmInit(StorageManager *pMgr, MMResult *pmmResult){
-    
+
 }
 
 void * mmAllocate(StorageManager *pMgr, short shDataSize, short shNodeType, char sbData[], MMResult *pmmResult){
 }
 
 void mmMark(StorageManager *pMgr, MMResult *pmmResult){
+    //sets the shSize to 0 as an initialization step
+    short shSizeCurrNodeCount = 0;
+    for(char * cursor = pMgr->pBeginStorage;cursor != pMgr->pEndStorage;cursor++)//increments the cursor by 1 byte
+    {
+        if(shSizeCurrNodeCount == 0)
+        {
+            cursor->cgC = 'C';
+            shSizeCurrNodeCount = cursor->shNodeSize;
+        }
+        else
+            shSizeCurrNodeCount--;
+    }
 }
 
 void mmFollow(StorageManager *pMgr, void *pUserData, MMResult *pmmResult){
+    char * followed[500];//keeps track of all the nodes that have been read completely
+    //pMgr->metaAttrM
 }
 
 void mmCollect(StorageManager *pMgr, MMResult *pmmResult){
+    
+    //sets free nodes to F from C
 }
 
 void mmAssoc(StorageManager *pMgr, void *pUserDataFrom, char szAttrName[], void *pUserDataTo, MMResult *pmmResult){
