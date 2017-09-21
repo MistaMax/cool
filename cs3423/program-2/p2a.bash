6843f1502131ff6bf6e92c6982c483ba89d143c9
Max Crookshanks <max@localhost.localdomain>
2017-09-21 00:31:02 -0500

    coded most of program 2 for systems programming

--- cs3423/program-2/p2a.bash
+++ cs3423/program-2/p2a.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for ln in $(grep -f "**Never logged in**" lastlog1.out);do
-    sed p2a.sed
-done
+cat lastlog1.out lastlog2.out | sed '/2017$/d;/^Username/d' | sort | sed -f p2aDollar.sed | sed -f p2a.sed | sed 's/\$$//' >p2a.out
+
+#grep -Fwh "**Never logged in**" *.out | sort | sed -f p2aDollar.sed | sed -f p2a.sed >result.txt
--- cs3423/program-2/p2b.bash
+++ cs3423/program-2/p2b.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+cat lastlog1.out lastlog2.out | sed '/2017$/d;/^Username/d' | sort | sed 's/\(.\{1,15\}\).*/\1/;s/[[:blank:]]//g' | uniq -c | sed -f p2b.sed >p2b.out
