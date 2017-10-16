7dd79e629aae47e90153f6fb2fd0623d97187749
Max Crookshanks <mmc_the_ace@yahoo.com>
2017-10-16 13:32:16 -0500

    did the project sorter for program-6

--- cs3423/program-6/projectSorter.pl
+++ cs3423/program-6/projectSorter.pl
@@ -1,3 +1,37 @@
 #!/usr/bin/perl -w
 use strict;
 
+#if($#ARGV == -1)
+#{
+#    die "Not enough args";
+#}
+
+my $dir = "/home/max/git/schoolCode/cs3423/program-6/DataB";
+
+if (! -d $dir)
+{
+    die "Not a directory\n";
+}
+
+my $dirName;
+
+my @filesArr = glob($dir);
+
+foreach my $file (@filesArr)
+{
+    my $assignment = $file;
+    if($assignment =~ m/^proj/)
+    {
+        $assignment =~ s/^proj|\..*$//g;
+        $dirName = join("","assignment",$assignment);
+    }
+    else
+    {
+        $dirName = "misc";
+    }
+    if (! -d $dirName)
+    {
+        print `mkdir $dirName`;
+    }
+    print `cp $dir/$file $dirName`;
+}
