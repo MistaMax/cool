73aef41ae2c475f740667aff52ff428b0ee53abb
MistaMax <mmc_the_ace@yahoo.com>
2017-11-09 15:08:28 -0600

    did the extra credit for program-7

--- cs3423/program-7/xdir.c
+++ cs3423/program-7/xdir.c
@@ -5,131 +5,19 @@
 #include <errno.h>
 #include <string.h>
 #include <dirent.h>
-
+//
 void errExit(const char szFmt[], ...);
-void processCommandSwitches(int argc, char *argv[], struct dirent *pDirent, struct stat statBuf);
-void listAllFiles(char *szDirName, struct dirent *pDirent, struct stat statBuf);
-void listFileSize(char *szDirName, struct dirent *pDirent, struct stat statBuf);
-void listFiles(char *szDirName, struct dirent *pDirent, struct stat statBuf);
-void listAllFileData(char *szDirName, struct dirent *pDirent, struct stat statBuf);
-char *concat(const char *s1, const char *s2);
-
+void listAllFiles(char *szDirName);
+void listFileSize(char *szDirName);
+void listFiles(char *szDirName);
+void listAllFileData(char *szDirName);
+//char *concat(const char *s1, const char *s2);
+void listFileDataR(char *szDirName, int indent);
+void listAllFileDataR(char *szDirName, int indent);
+void listAllFilesR(const char *szDirName, int indent);
+void listFilesR(const char *szDirName, int indent);
 //
 int main(int argc, char *argv[])
-{
-	struct dirent *pDirent;
-	struct stat statBuf;
-
-	processCommandSwitches(argc, argv, pDirent, statBuf);
-
-	return 0;
-}
-//
-void listFiles(char *szDirName, struct dirent *pDirent, struct stat statBuf)
-{
-	DIR *pDir;
-
-	pDir = opendir(szDirName);
-
-	if (pDir == NULL)
-		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
-
-	printf("%s :\n", szDirName);
-	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		if (pDirent->d_name[0] != '.')
-		{
-			printf("\t%s\n", pDirent->d_name);
-		}
-	}
-	close(pDir);
-}
-//
-void listAllFiles(char *szDirName, struct dirent *pDirent, struct stat statBuf)
-{
-	DIR *pDir;
-
-	pDir = opendir(szDirName);
-
-	if (pDir == NULL)
-		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
-
-	printf("%s :\n", szDirName);
-	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		printf("\t%s\n", pDirent->d_name);
-	}
-	close(pDir);
-}
-//
-void listFileSize(char *szDirName, struct dirent *pDirent, struct stat statBuf)
-{
-	DIR *pDir;
-	int rc = 0;
-	pDir = opendir(szDirName);
-
-	char *szTmp;
-
-	if (pDir == NULL)
-		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
-
-	printf("%s:\n", szDirName);
-	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		szTmp = concat(szDirName, "/");
-		szTmp = concat(szTmp, pDirent->d_name);
-		rc = stat(szTmp, &statBuf);
-		if (rc < 0)
-		{
-			errExit("stat: %s", strerror(errno));
-		}
-
-		if (pDirent->d_name[0] != '.')
-		{
-			if (S_ISDIR(statBuf.st_mode))
-			{
-				printf("\t%s D %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
-			}
-			if (S_ISREG(statBuf.st_mode))
-				printf("\t%s F  %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
-		}
-	}
-	close(pDir);
-}
-//
-void listAllFileData(char *szDirName, struct dirent *pDirent, struct stat statBuf)
-{
-	DIR *pDir;
-	int rc = 0;
-	pDir = opendir(szDirName);
-
-	char *szTmp;
-
-	if (pDir == NULL)
-		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
-
-	printf("%s:\n", szDirName);
-	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		szTmp = concat(szDirName, "/");
-		szTmp = concat(szTmp, pDirent->d_name);
-		rc = stat(szTmp, &statBuf);
-		if (rc < 0)
-		{
-			errExit("stat: %s", strerror(errno));
-		}
-
-		if (S_ISDIR(statBuf.st_mode))
-		{
-			printf("\t%s D %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
-		}
-		if (S_ISREG(statBuf.st_mode))
-			printf("\t%s F  %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
-	}
-	close(pDir);
-}
-//
-void processCommandSwitches(int argc, char *argv[], struct dirent *pDirent, struct stat statBuf)
 {
 	int i;
 
@@ -140,73 +28,322 @@ void processCommandSwitches(int argc, char *argv[], struct dirent *pDirent, stru
 		errExit("Switch should not be the first argument\n");
 
 	if (argc == 2)
-	{
-		listFiles(argv[1], pDirent, statBuf);
-	}
-	else if (argc == 3)
-	{
-		//
-		if (argv[2][0] == '-')
-		{
-			if (argv[2][1] == 'l')
-			{
-				listFileSize(argv[1], pDirent, statBuf);
-			}
-			else if (argv[2][1] == 'a')
-			{
-				listAllFiles(argv[1], pDirent, statBuf);
-			}
-		}
-	}
+		listFiles(argv[1]);
 	else
 	{
 		//
 		int l = 0;
 		int a = 0;
+		int r = 0;
 		for (i = 2; i < argc; i++)
 		{
 			if (argv[i][0] == '-')
 			{
 				if (argv[i][1] == 'l')
-				{
 					l = 1;
-				}
 				else if (argv[i][1] == 'a')
-				{
 					a = 1;
-				}
-			}
-		}
-		//
-		if (l == 1)
-		{
-			if (a == 1)
-			{
-				listAllFileData(argv[1], pDirent, statBuf);
+				else if (argv[i][1] == 'r')
+					r = 1;
+				else
+					errExit("Invalid command argument");
 			}
 			else
 			{
-				listFileSize(argv[1], pDirent, statBuf);
+				errExit("Improper argument");
 			}
 		}
-		else
+
+		//
+		if (l)
 		{
-			if (a == 1)
-			{
-				listAllFiles(argv[1], pDirent, statBuf);
+			if (a){
+				if (r)
+					listAllFileDataR(argv[1], 4);
+				else
+					listAllFileData(argv[1]);
+			}
+			else{
+				if (r)
+					listFileDataR(argv[1], 4);
+				else
+					listFileSize(argv[1]);
 			}
 		}
+		else if (a)
+		{
+			if (r)
+				listAllFilesR(argv[1], 4);
+			else
+				listAllFiles(argv[1]);
+		}
+		else if (r)
+			listFilesR(argv[1], 4);
 	}
+
+	return 0;
 }
 
-char *concat(const char *s1, const char *s2)
+//
+void listFiles(char *szDirName)
 {
-	char *result = malloc(strlen(s1) + strlen(s2) + 1); //+1 for the null-terminator
-	if (result == NULL)
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	DIR *pDir;
+	pDir = opendir(szDirName);
+
+	if (pDir == NULL)
+		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
+
+	printf("%s :\n", szDirName);
+	while ((pDirent = readdir(pDir)) != NULL)
 	{
-		errExit("Malloc Failed");
+		if (pDirent->d_name[0] != '.')
+		{
+			printf("    %s\n", pDirent->d_name);
+		}
 	}
-	strcpy(result, s1);
-	strcat(result, s2);
-	return result;
+	close(pDir);
+}
+//
+void listAllFiles(char *szDirName)
+{
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	DIR *pDir;
+
+	pDir = opendir(szDirName);
+
+	if (pDir == NULL)
+		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
+
+	printf("%s :\n", szDirName);
+	while ((pDirent = readdir(pDir)) != NULL)
+	{
+		printf("    %s\n", pDirent->d_name);
+	}
+	close(pDir);
+}
+//
+void listFileSize(char *szDirName)
+{
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	DIR *pDir;
+	int rc = 0;
+	pDir = opendir(szDirName);
+
+	char path[500];
+
+	if (pDir == NULL)
+		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
+	
+	printf("%s:\n", szDirName);
+	while ((pDirent = readdir(pDir)) != NULL)
+	{
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+		if (pDirent->d_name[0] != '.')
+		{
+			if (S_ISDIR(statBuf.st_mode))
+			{
+				printf("    %s D %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+			}
+			if (S_ISREG(statBuf.st_mode))
+				printf("    %s F  %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+		}
+	}
+	close(pDir);
+}
+//
+void listAllFileData(char *szDirName)
+{
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	DIR *pDir;
+	int rc = 0;
+	pDir = opendir(szDirName);
+
+	char path[500];
+
+	if (pDir == NULL)
+		errExit("opendir could not open '%s': %s", szDirName, strerror(errno));
+
+	printf("%s:\n", szDirName);
+	while ((pDirent = readdir(pDir)) != NULL)
+	{
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+		if (S_ISDIR(statBuf.st_mode))
+		{
+			printf("    %s D %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+		}
+		if (S_ISREG(statBuf.st_mode))
+			printf("    %s F  %ld blks %ld bytes\n", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+	}
+	close(pDir);
+}
+//
+void listFilesR(const char *szDirName, int indent)
+{
+    DIR *dir;
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	int rc = 0;
+
+	if(indent == 4){
+		printf("%s:\n",szDirName);
+	}
+
+    if (!(dir = opendir(szDirName)))
+        return;
+
+    while ((pDirent = readdir(dir)) != NULL) {
+		char path[500];
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+        if (S_ISDIR(statBuf.st_mode)) {
+            if (pDirent->d_name[0] == '.')
+                continue;
+            printf("%*s%s:\n", indent, " ", pDirent->d_name);
+            listFilesR(path, indent + 4);
+        } else {
+			if (pDirent->d_name[0] == '.')
+				continue;
+            printf("%*s%s\n", indent, " ", pDirent->d_name);
+        }
+    }
+    closedir(dir);
+}
+//
+void listAllFilesR(const char *szDirName, int indent)
+{
+    DIR *dir;
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	int rc = 0;
+
+	if(indent == 4){
+		printf("%s:\n",szDirName);
+	}
+
+    if (!(dir = opendir(szDirName)))
+        return;
+
+    while ((pDirent = readdir(dir)) != NULL) {
+		char path[500];
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+        if (S_ISDIR(statBuf.st_mode)) {
+			if (strcmp(pDirent->d_name, ".") == 0 || strcmp(pDirent->d_name, "..") == 0){
+				printf("%*s%s\n", indent, " ", pDirent->d_name);
+				continue;
+			}
+            printf("%*s%s:\n", indent, " ", pDirent->d_name);
+            listAllFilesR(path, indent + 4);
+        } else {
+            printf("%*s%s\n", indent, " ", pDirent->d_name);
+        }
+    }
+    closedir(dir);
+}
+//
+void listAllFileDataR(char *szDirName, int indent){
+    DIR *dir;
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	int rc = 0;
+
+	if(indent == 4){
+		printf("%s:\n",szDirName);
+	}
+
+    if (!(dir = opendir(szDirName)))
+        return;
+
+    while ((pDirent = readdir(dir)) != NULL) {
+		char path[500];
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+        if (S_ISDIR(statBuf.st_mode)) {
+			if (strcmp(pDirent->d_name, ".") == 0 || strcmp(pDirent->d_name, "..") == 0){
+				printf("%*s%s D %ld blks %ld bytes\n", indent, " ", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+				continue;
+			}
+            printf("%*s%s:\n", indent, " ", pDirent->d_name);
+            listAllFileDataR(path, indent + 4);
+        } else {
+            printf("%*s%s F %ld blks %ld bytes\n", indent, " ", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+        }
+    }
+    closedir(dir);
+}
+//
+void listFileDataR(char *szDirName, int indent){
+	DIR *dir;
+	struct dirent *pDirent;
+	struct stat statBuf;
+
+	int rc = 0;
+
+	if(indent == 4){
+		printf("%s:\n",szDirName);
+	}
+
+    if (!(dir = opendir(szDirName)))
+        return;
+
+    while ((pDirent = readdir(dir)) != NULL) {
+		char path[500];
+		snprintf(path, sizeof(path), "%s/%s", szDirName, pDirent->d_name);
+		rc = stat(path, &statBuf);
+		if (rc < 0)
+		{
+			errExit("stat: %s", strerror(errno));
+		}
+
+        if (S_ISDIR(statBuf.st_mode)) {
+			if (pDirent->d_name[0] == '.')
+				continue;
+            printf("%*s%s:\n", indent, " ", pDirent->d_name);
+            listFileDataR(path, indent + 4);
+        } else {
+			if (pDirent->d_name[0] == '.')
+				continue;
+            printf("%*s%s F %ld blks %ld bytes\n", indent, " ", pDirent->d_name, statBuf.st_blocks, statBuf.st_size);
+        }
+    }
+    closedir(dir);
 }
