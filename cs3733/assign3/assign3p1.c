d20f251b94429e323c9bc701a739aa730ad530e0
Max Crookshanks <mmc_the_ace@yahoo.com>
2018-04-02 20:25:30 -0500

    commenting assign3p1

--- cs3733/assign3/assign3p1.c
+++ cs3733/assign3/assign3p1.c
@@ -9,23 +9,23 @@ int main(int argc, char *argv[]) {
    int addrtrans[] = {2, 4, 1, 7, 3, 5, 6};
    int fdin, fdout;
    unsigned long offset, page, physaddr, virtaddr;
-   
+   //checks to see if there are the right ammount of arguments
    if (argc != 2) {
       printf("Usage: %s <SEQUENCE FILE>\n", argv[0]);
       exit(1);
    }
-   
+   //opens the input file
    if ((fdin = open(argv[1], O_RDONLY)) == -1) {
       perror("Main failed to open sequence file");
       exit(2);
    }
-   
+   //Creates the output file
    if ((fdout = open("output-part1", O_WRONLY | O_CREAT, 0600)) == -1) {
       perror("Main failed to open output file");
       close(fdin);
       exit(2);
    }
-   
+   //reads through the file and edits the page table
    while (read(fdin, &virtaddr, sizeof(virtaddr)) > 0) {
       offset = virtaddr & 0x7F;
       page = virtaddr >> 7;
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
          exit(3);
       }
    }
-   
+   //closing files
    close(fdin);
    close(fdout);
    
